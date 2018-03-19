2a8d2aa9d7942f475e951733c8ffdbf296ea85dd
github username <github email address>
2018-03-19 16:14:17 -0400

    Done.

--- js/init.js
+++ js/init.js
@@ -1,12 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var API_KEY = "Go get an API key";
+  var API_KEY = "72c8f49d1b13f0d2";
   var CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
   var URL = CORS_WRAPPER + "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
-  var ctx = document.getElementById("NYCWeatherChart").getContext("2d")
 
 
-  makeRequest(URL, function(json) {
-    var data = generateDataSet(getHours(json), getFahrenheits(json));
-    var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
-  });
+  // makeRequest(URL, function(json) {
+  //   var data = generateDataSet(getHours(json), getFahrenheits(json));
+  //   var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
+  // });
+
+
+  function makeRequest(endpoint, success) {
+    // Your code goes here
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(json => generateDataSet(getHours(json), getFahrenheits(json)))
+  }
+
+  makeRequest(URL)
+
 });
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,45 @@
 function getFahrenheits(result){
   // Your code goes here
+  return result.hourly_forecast.map(function(f) {
+    return f.temp.english;
+  })
 }
 
 function getHours(result){
   // Your code goes here
+  return result.hourly_forecast.map(function(hours) {
+    return hours.FCTTIME.hour;
+  })
 }
 
 function generateDataSet(labels, data) {
   // Your code goes here
-}
-
-function makeRequest(endpoint, success) {
-  // Your code goes here
+  let ctx = document.getElementById("NYCWeatherChart").getContext("2d")
+  let myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: "dunno",
+        data: data,
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+    }]
+    }
+  })
 }
