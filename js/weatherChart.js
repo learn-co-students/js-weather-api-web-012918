3690ccb4dd92746863cf4f19b80cbcc9454b0d9f
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-03-19 15:02:50 -0400

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,63 @@
 function getFahrenheits(result){
-  // Your code goes here
+  let hourly = result.hourly_forecast
+  return hourly.map((hour) => {
+    return parseInt(hour.temp.english)
+  });
 }
 
 function getHours(result){
-  // Your code goes here
+  let hourly = result.hourly_forecast
+  return hourly.map((hour) => {
+    return parseInt(hour.FCTTIME.hour)
+  });
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  let ctx = document.getElementById("NYCWeatherChart");
+  let myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          labels: labels
+          ,
+          datasets: [{
+              label: 'Hourly Temperature',
+              data: data,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)'
+
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)'
+              ],
+              borderWidth: 1
+          }]
+      }
+      ,
+      options: {
+        title: {
+            display: true,
+            text: 'NYC Hourly Weather'
+        },
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
 }
 
-function makeRequest(endpoint, success) {
-  // Your code goes here
+var API_KEY = "8cd97c107d1e0717";
+var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
+
+function getWeather(){
+  fetch(URL)
+  .then(res => res.json())
+  .then(json => {
+    generateDataSet(getHours(json),getFahrenheits(json))
+  })
 }
+
+window.addEventListener('DOMContentLoaded', getWeather)
