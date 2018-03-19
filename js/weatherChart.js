e69d5f865eebc68758882f67edfc33326c545c41
porzingod <jasonlim.ny@gmail.com>
2018-03-19 15:38:37 -0400

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,33 @@
 function getFahrenheits(result){
   // Your code goes here
+  return result.hourly_forecast.map(x => x.temp.english)
 }
 
 function getHours(result){
   // Your code goes here
+  return result.hourly_forecast.map(x => x.FCTTIME.hour)
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  return {
+    labels: labels, //getHours(result)
+    datasets: [
+    {
+      label: "Hourly Weather for New York",
+      fillColor: "rgba(220,220,220,0.2)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: data //getFahrenheits(result)
+    }
+    ]
+  }
 }
 
 function makeRequest(endpoint, success) {
-  // Your code goes here
+  fetch(endpoint)
+  .then(res => res.json())
+  .then(json => success(json))
 }
