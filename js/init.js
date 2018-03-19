1f38d930f47e44384de40dddeefb6a18ad580973
scott <you@example.comitsmescottb123@gmail.com>
2018-03-19 15:28:57 -0400

    Done.

--- js/init.js
+++ js/init.js
@@ -1,12 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var API_KEY = "Go get an API key";
+  var API_KEY = "f9db2c4b9a0ac69d";
   var CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
-  var URL = CORS_WRAPPER + "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
-  var ctx = document.getElementById("NYCWeatherChart").getContext("2d")
+  var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
 
+  function makeRequest(endpoint, success) {
+    // Your code goes here
+    fetch(endpoint).then(
+      resp => {resp.json().then(
+        data => {
+          generateDataSet(getHours(data), getFahrenheits(data))
+        }
+      )}
+    )
+  }
+  // makeRequest(URL, function(json) {
+  //   var data = generateDataSet(getHours(json), getFahrenheits(json));
+  //   var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
+  // });
 
-  makeRequest(URL, function(json) {
-    var data = generateDataSet(getHours(json), getFahrenheits(json));
-    var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
-  });
+  makeRequest(URL)
 });
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,62 @@
 function getFahrenheits(result){
   // Your code goes here
+  return result.hourly_forecast.map(function(x) {
+    console.log(x['temp']["english"])
+    return x['temp']["english"]
+  })
 }
 
 function getHours(result){
   // Your code goes here
+  return result.hourly_forecast.map(function(x) {
+    console.log(x['FCTTIME']['hour'])
+    return x['FCTTIME']['hour']
+  })
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+ let ctx = document.getElementById("NYCWeatherChart");
+ let myChart = new Chart(ctx, {
+     type: 'line',
+     data: {
+         labels: labels
+         ,
+         datasets: [{
+             label: 'Hourly Temperature',
+             data: data,
+             backgroundColor: [
+                 'rgba(255, 99, 132, 0.2)'
+
+             ],
+             borderColor: [
+                 'rgba(255,99,132,1)'
+             ],
+             borderWidth: 1
+         }]
+     }
+     ,
+     options: {
+       title: {
+           display: true,
+           text: 'NYC Hourly Weather'
+       },
+         scales: {
+             yAxes: [{
+                 ticks: {
+                     beginAtZero:true
+                 }
+             }]
+         }
+     }
+ });
 }
 
-function makeRequest(endpoint, success) {
-  // Your code goes here
-}
+// function getWeather(){
+//  fetch(URL)
+//  .then(res => res.json())
+//  .then(json => {
+//    generateDataSet(getHours(json),getFahrenheits(json))
+//  })
+// }
+//
+// window.addEventListener('DOMContentLoaded', getWeather)
