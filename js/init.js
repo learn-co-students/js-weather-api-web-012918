b4616c6d1c87a1a077ef4262ca881427e016b75a
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-03-19 15:29:57 -0400

    Done.

--- js/init.js
+++ js/init.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var API_KEY = "Go get an API key";
-  var CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
-  var URL = CORS_WRAPPER + "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
+  var API_KEY = "71bb67004e9aefac";
+  var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
   var ctx = document.getElementById("NYCWeatherChart").getContext("2d")
-
 
   makeRequest(URL, function(json) {
     var data = generateDataSet(getHours(json), getFahrenheits(json));
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,40 @@
+
 function getFahrenheits(result){
-  // Your code goes here
+  // console.log(result["hourly_forecast"].map(el => el["temp"]["english"]))
+  return result["hourly_forecast"].map(el => el["temp"]["english"])
 }
 
 function getHours(result){
-  // Your code goes here
+  console.log(result["hourly_forecast"].map(el => el["FCTTIME"]["hour"]))
+  return result["hourly_forecast"].map(el => el["FCTTIME"]["hour"])
 }
 
-function generateDataSet(labels, data) {
-  // Your code goes here
-}
 
 function makeRequest(endpoint, success) {
-  // Your code goes here
+  $.ajax({
+  url: endpoint,
+  dataType : 'jsonp',
+  success: success
+  });
+}
+
+
+
+
+
+function generateDataSet(labels, data) {
+  return {
+  	labels: labels,
+  	datasets: [
+  	{
+  		label: "Hourly Weather for New York",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: data
+  	}]
+  }
 }
