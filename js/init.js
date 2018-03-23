154a0bc1785c0c9eed77e2e77eb6b6335119ea60
harenarium <22226614+harenarium@users.noreply.github.com>
2018-03-22 23:20:14 -0400

    Done.

--- js/init.js
+++ js/init.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var API_KEY = "Go get an API key";
+  var API_KEY = "19ab6da8c1f34049";
   var CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
-  var URL = CORS_WRAPPER + "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
+  var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
   var ctx = document.getElementById("NYCWeatherChart").getContext("2d")
 
 
-  makeRequest(URL, function(json) {
+  makeAjaxRequest(URL, function(json) {
     var data = generateDataSet(getHours(json), getFahrenheits(json));
     var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
   });
